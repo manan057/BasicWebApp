30c827ceaa292b9df987636a845c1a00e272b2c8
Manan <manan_rana@hotmail.com>
2021-08-24 01:08:45 +1000

    created carousel and created some content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import './App.css';
+import { Typography, Divider } from 'antd';
+import PositionCarouselDemo from "./Carousel";
+const { Title, Paragraph, Text, Link } = Typography;
 
 const { Header, Content, Footer } = Layout;
 
@@ -17,15 +20,20 @@ function App() {
           <Menu.Item key="3">nav 3</Menu.Item>
         </Menu>
       </Header>
-      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-          Content
-        </div>
+      <PositionCarouselDemo />
+      <Content className="site-layout" style={{ padding: '0 80px', marginTop: 64 }}>
+        <Typography>
+          <Title>Introduction</Title>
+          <Paragraph>
+            After massive project practice and summaries, <Text code>Ant Design</Text>, a design language for background
+            applications, is refined by Ant UED Team, which aims to
+            <Text strong>
+              uniform the user interface specs for internal background projects, lower the unnecessary
+              cost of design differences and implementation and liberate the resources of design and
+              front-end development
+            </Text>.
+          </Paragraph>
+        </Typography>
       </Content>
       <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
     </Layout>
